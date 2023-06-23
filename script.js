c1f13afe8f389b4718c432a019192503d3cfe969
Iwan <email>
2023-06-22 21:42:13 -0300

    :tada

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //Pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //Se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    //Se stiver sem light mode, manter imagem normal
+    img.setAttribute("src", "*/assets/avatar.png")
+  }
+}
